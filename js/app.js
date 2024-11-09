0a4d668dcd807ee910d2c8118c812318544e7a2c
carxdetail <carxdetailing101@gmail.com>
2024-11-08 21:38:54 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -8,14 +8,14 @@ const back_s1 = document.querySelector('.mechanic>.back_s1')
 
 function opt_clicked_animation() {
     opt_esthetic.style.transition = '0.7s ease-in'
-    opt_mechanic.style.transition = '0.5s ease-in'
+    // opt_mechanic.style.transition = '0.5s ease-in'
     s1_h1.style.transition = '1s ease-in'
     setTimeout(()=>{
         s1_h1.style.transform = `translateX(-${innerWidth}px)`
         setTimeout(()=>{
             opt_esthetic.style.transform = `translateY(${innerHeight}px)`
             setTimeout(()=>{
-                opt_mechanic.style.transform = `translateY(-${innerHeight}px)`
+                // opt_mechanic.style.transform = `translateY(-${innerHeight}px)`
             },75)
         },125)
     },50)
@@ -23,25 +23,25 @@ function opt_clicked_animation() {
 
 function back_s1_animation() {
     opt_esthetic.style.transition = ''
-    opt_mechanic.style.transition = ''
+    // opt_mechanic.style.transition = ''
     s1_h1.style.transition = '0.1 ease-out'
     setTimeout(()=>{
         s1_h1.style.transform = ''
         setTimeout(()=>{
             opt_esthetic.style.transform = ''
             setTimeout(()=>{
-                opt_mechanic.style.transform = ''
+                // opt_mechanic.style.transform = ''
             },75)
         },125)
     },50)
 }
 
-opt_mechanic.addEventListener('click', () => {
-    opt_clicked_animation()
-    setTimeout(()=>{
-        mechanic.style.transform = 'translate(0%, 0%) scale(1)'
-    },250)
-})
+// opt_mechanic.addEventListener('click', () => {
+    // opt_clicked_animation()
+    // setTimeout(()=>{
+        // mechanic.style.transform = 'translate(0%, 0%) scale(1)'
+    // },250)
+// })
 
 opt_esthetic.addEventListener('click', () => {
     opt_clicked_animation()
@@ -51,7 +51,7 @@ opt_esthetic.addEventListener('click', () => {
 })
 
 back_s1.addEventListener('click', () => {
-    mechanic.style.transform = ''
+    // mechanic.style.transform = ''
     back_s1_animation()
 })
 
