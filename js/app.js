19fae57a98fddc00796e52fada3e0e0afdd7ad04
carxdetail <carxdetailing101@gmail.com>
2024-11-08 21:09:26 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@ const s1_h1 = document.querySelector('.s1>h1')
 const opt_esthetic = document.querySelector('.s1>.opts>.opt1')
 const opt_mechanic = document.querySelector('.s1>.opts>.opt2')
 const mechanic = document.querySelector('.mechanic')
+const esthetic = document.querySelector('.esthetic')
+const interior_detail = document.querySelector('.esthetic>.opts>.opt1')
 const back_s1 = document.querySelector('.mechanic>.back_s1')
 
 function opt_clicked_animation() {
@@ -20,9 +22,9 @@ function opt_clicked_animation() {
 }
 
 function back_s1_animation() {
-    opt_esthetic.style.transition = '0.7s ease-in'
-    opt_mechanic.style.transition = '0.5s ease-in'
-    s1_h1.style.transition = '1s ease-in'
+    opt_esthetic.style.transition = ''
+    opt_mechanic.style.transition = ''
+    s1_h1.style.transition = '0.1 ease-out'
     setTimeout(()=>{
         s1_h1.style.transform = ''
         setTimeout(()=>{
@@ -41,7 +43,20 @@ opt_mechanic.addEventListener('click', () => {
     },250)
 })
 
+opt_esthetic.addEventListener('click', () => {
+    opt_clicked_animation()
+    setTimeout(()=>{
+        esthetic.style.transform = 'translate(0%, 0%) scale(1)'
+    },250)
+})
+
 back_s1.addEventListener('click', () => {
     mechanic.style.transform = ''
     back_s1_animation()
+})
+
+interior_detail.addEventListener('click', () => {
+    // https://buy.stripe.com/cN27sT4OZg4A6Yw5kk
+    // window.location.href = "https://buy.stripe.com/cN27sT4OZg4A6Yw5kk"
+    window.open('https://buy.stripe.com/cN27sT4OZg4A6Yw5kk', '_blank')
 })
